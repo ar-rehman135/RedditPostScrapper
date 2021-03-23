88640b404ce9ee981838e1adb789d9056deb53a5
abdulrehmanmirza <abdulrehmanmirza135@gmail.com>
2021-03-23 11:14:44 +0000

    cronjobs add

---
+++ cronjob.sh
@@ -0,0 +1,1 @@
+python3 /home/reddit/redit_data/RedditPostScrapper/main.py 2>&1 1>/home/reddit/redit_data/RedditPostScapper/runlog.txt
---
+++ cronjob1.sh
@@ -0,0 +1,1 @@
+python3 /home/reddit/redit_data/RedditPostScrapper/main.py --getpolygondata 2>&1> 1>/home/reddit/redit_data/RedditPostScrapper/runlog1.txt
