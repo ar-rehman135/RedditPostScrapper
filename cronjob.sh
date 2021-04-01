10404212c45da30088457c0a3a8d81a390fc4dad
abdulrehmanmirza <abdulrehmanmirza135@gmail.com>
2021-04-01 14:09:57 +0000

    update cronjobs

--- cronjob.sh
+++ cronjob.sh
@@ -1,1 +1,2 @@
-python3 /home/reddit/redit_data/RedditPostScrapper/main.py 2>&1 1>/home/reddit/redit_data/RedditPostScapper/runlog.txt
+source /home/reddit/redit_data/RedditPostScrapper/venv/bin/activate 
+python3 /home/reddit/redit_data/RedditPostScrapper/main.py 2>&1 1>/home/reddit/redit_data/RedditPostScrapper/runlog.txt
--- cronjob1.sh
+++ cronjob1.sh
@@ -1,1 +1,2 @@
+source /home/reddit/redit_data/RedditPostScrapper/venv/bin/activate
 python3 /home/reddit/redit_data/RedditPostScrapper/main.py --getpolygondata 2>&1> 1>/home/reddit/redit_data/RedditPostScrapper/runlog1.txt
